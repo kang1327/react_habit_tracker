a33098b72656842e771df9ed5813990688d84970
unknown <kang1327@gamil.com>
2021-03-23 00:06:56 +0900

    start

---
+++ src/components/login/login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+
+const Login = ({authService}) => {
+    return (
+        <section>
+            <Header />
+                <section>
+                    <h1>Login</h1>
+                    <ul>
+                        <li><button>Goggle</button></li>
+                        <li><button>Github</button></li>
+                    </ul>
+                </section>
+            
+            <Footer />
+        </section>
+    )
+}
+
+export default Login;
